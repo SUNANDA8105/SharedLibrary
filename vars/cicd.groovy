c1d11f672e579d756d46d01c0d812c2957025b71
SUNANDA8105 <164487864+SUNANDA8105@users.noreply.github.com>
2024-04-18 16:53:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,5 +6,5 @@ sh 'mvn package'
 }
 def newDeploy(ip,appname){
   deploy adapters: [tomcat9(credentialsId: '8e0a0c6d-92d5-454c-8750-dd2db08e6daf', path: '', 
-                            url: "${http://172.31.94.253:8080}")], contextPath: "${testapp}", war: '**/*.war'
+                            url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
                             }
