9ca2130b67722a325b96506851b8ff62c0b06a3e
SUNANDA8105 <164487864+SUNANDA8105@users.noreply.github.com>
2024-04-18 16:50:58 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -4,3 +4,7 @@ git "${repo}"
 def newMaven(){
 sh 'mvn package'
 }
+def newDeploy(ip,appname){
+  deploy adapters: [tomcat9(credentialsId: '8e0a0c6d-92d5-454c-8750-dd2db08e6daf', path: '', 
+                            url: "${http://172.31.94.253:8080}")], contextPath: "${testapp}", war: '**/*.war'
+                            }
